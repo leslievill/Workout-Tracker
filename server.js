00b733c5c583dceae6ad06468cc53c1683a5aafc
Leslie Villatoro <levill98@gmail.com>
2020-11-05 18:46:47 -0800

    minor typo

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`🌎 App running on port ${PORT}!`);
 });
